1061c717a09fd3bc54ef5357dfab9aa67517f00a
Vladimir Leonov <fatur_muhammad@bk.ru>
2026-04-01 22:29:26 +0700

    fix drop item

--- src/hooks/drop_item.cpp
+++ src/hooks/drop_item.cpp
@@ -6,15 +6,13 @@
 namespace big
 {
     bool_command _enable_override_drop("override_drop", "Override Drop", "Override drop item value.", false);
-    int_command _drop_amount("drop_amount", "", "", 100.f, 10000.f, 100.f);
+    int_command _drop_amount("drop_amount", "", "", 1, 100, 1);
 
 	MonoObject* hooks::drop_item(MonoObject* item, int amount, Vector3 position, Quaternions rotation)
 	{
         TRY_CLAUSE
         {
             item_data itm(item);
-
-            LOG(INFO) << "Location: " << position.x << ", " << position.y << ", " << position.z;
 
             auto ret = detour_base::get_original<hooks::drop_item>()(
                 item,
